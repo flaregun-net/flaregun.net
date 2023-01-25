02604d20604149d0e29e3d6fe990c2c48bcaf57c
Network Chimp <hello@networkchimp.com>
2023-01-25 11:27:01 -0600

    updated homepage styles

--- src/components/HomepageDiscord/index.tsx
+++ src/components/HomepageDiscord/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import Link from "@docusaurus/Link"
-import DiscordIcon from "@site/static/icons/discord.svg"
+import clsx from "clsx"
+import React from "react"
 import styles from "./styles.module.scss"
 
 export function HomepageDiscord() {
   return (
-    <div className="hero hero--dark">
+    <div
+      style={{ marginTop: 40 }}
+      className={clsx(["hero hero--dark", styles.wrapper])}
+    >
       <div className="container">
         <div className="row">
           <div className="col col--12 flex-center">
@@ -14,12 +17,11 @@ export function HomepageDiscord() {
               Questions or feedback? Hop in our Discord to chat.
             </p>
             <Link
-              className="button button--primary button--inline-block"
+              className="button button--secondary button--inline-block"
               href="https://discord.gg/ZqeZsA3qCT"
               rel="noopener noreferrer"
             >
               <div className={styles.textIconLink}>
-                <DiscordIcon width={20} height={20} />
                 <span>Join Flaregun</span>
               </div>
             </Link>
